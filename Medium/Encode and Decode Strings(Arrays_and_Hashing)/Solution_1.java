66d728c1590d5e0334651d19e3c5c073c64d12db
Bisrat Kassie <107273623+bisrat415@users.noreply.github.com>
2023-06-07 18:01:42 -0400

    minor fixes on Encode and Decode

--- Medium/Encode and Decode Strings(Arrays_and_Hashing)/Solution_1.java
+++ Medium/Encode and Decode Strings(Arrays_and_Hashing)/Solution_1.java
@@ -1,3 +1,4 @@
+// This solution uses non-ASCII unichar character as a delimeter (any char with ASCII number above 256)
 import java.util.*;
 
 public class Solution_1 {
@@ -17,7 +18,7 @@ public class Solution_1 {
             return null;
         }
         String d = String.valueOf((char) 257);
-
+        // Create a string containing the values in strs separated by d
         StringBuilder sb = new StringBuilder();
         sb.append(strs.get(0));
         for (int i = 1; i < strs.size(); i++) {
@@ -33,7 +34,9 @@ public class Solution_1 {
             return new ArrayList<>();
         }
         String d = String.valueOf((char) 257);
+        // return the list of the words that are created by splitting the string s using d 
         return Arrays.asList(s.split(d, -1));
-
     }
+    // Time Complexity: O(n) - both encode and decode take O(n) time considering that split is O(n)
+	// Space Complexity: O(1) - sb in encode and the arraylist in decode are both part of the output so they won't be considered in the space analysis
 }
