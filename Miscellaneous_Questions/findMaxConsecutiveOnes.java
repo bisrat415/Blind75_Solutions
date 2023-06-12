402eb69634f395f65cc6f138ef04a41be35b3b0e
Bisrat Kassie <107273623+bisrat415@users.noreply.github.com>
2023-06-12 10:17:59 -0400

    comments and a test case added for findMaxConsecutiveOnes

--- Miscellaneous_Questions/findMaxConsecutiveOnes.java
+++ Miscellaneous_Questions/findMaxConsecutiveOnes.java
@@ -1,10 +1,12 @@
-package Miscellaneous_Questions;
-
+// This file solves Question 487 ( Max Consecutive Ones II) on leetcode
+// The question asks given a binary array nums, return the maximum number of consecutive 1's in the array if you can flip at most one 0
 public class findMaxConsecutiveOnes {
     public static void main(String[] args) {
-        
+        int[] nums = {1, 0, 1, 1, 0, 1, 1};
+        System.out.println(findMaxConsecutiveOnes(nums));
+
     }
-    public int findMaxConsecutiveOnes(int[] nums) {
+    public static int findMaxConsecutiveOnes(int[] nums) {
         int left = 0;
         int right = 0;
         int zeroCount = 0;
@@ -28,4 +30,7 @@ public class findMaxConsecutiveOnes {
         }
         return right - left > maxLength ? right - left : maxLength;
     }
+    // Time Complexity: O(n) - even though there is a nested while loop, the inner while loop only executes occasionally (when zeroCount > 1)
+    // Another justification is that since both the pointers only move forward, each of the left and right pointer traverse a maximum of n steps
+    // Space Complexity: O(1) - only constant extra space is used
 }
