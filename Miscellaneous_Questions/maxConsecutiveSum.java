6dd2c5308c28cfcd063463022b3b8d3e2a5e9252
Bisrat Kassie <107273623+bisrat415@users.noreply.github.com>
2023-06-13 11:04:43 -0400

    solution 1 for maxConsecutiveSum added in Miscellaneous

---
+++ Miscellaneous_Questions/maxConsecutiveSum.java
@@ -0,0 +1,25 @@
+// Question: Given an array of integers of size ‘n’, calculate the maximum sum possible for ‘k’ consecutive elements in the array.
+public class maxConsecutiveSum {
+    public static void main(String[] args) {
+        int[] nums = {100, 200, 300, 400};
+        int k = 2;
+        System.out.println(maxSum(nums, k));
+
+    }
+
+    // Solution #1 (brute-force)
+    public static int maxSum(int[] nums, int k) {
+        int maxSum = 0;
+        for (int i = 0; i <= nums.length - k; i++) {
+            int currSum = 0;
+            for (int j = 0; j < k; j++) {
+                currSum += nums[j + i];
+            }
+            maxSum = Math.max(maxSum, currSum);
+        }
+        return maxSum;
+        // Let n be the size of the array nums
+        // Time Complexity: O((n - k + 1) * k) because the outer loop runs (n - k + 1) and the inner loop iterates k times
+        // Space Complexity: O(1) because we are only using constant extra space (does not depend on the size of the input)
+    }
+}
