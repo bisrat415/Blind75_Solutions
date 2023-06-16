21f06a6df06a099b1e831106950e2459660a2a9a
Bisrat Kassie <107273623+bisrat415@users.noreply.github.com>
2023-06-16 11:38:11 -0400

    solution #1 added to Reorder List

---
+++ Medium/Reorder_List(Linked_List)/Solution_1.java
@@ -0,0 +1,67 @@
+// Step 1 - Find and go to the halfway point
+// Step 2 - Separate into two lists using the halfway point
+// Step 3 - reverse the second half
+// Step 4 - merge the two lists by going back and forth
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+public class Solution_1 {
+    public static void main(String[] args) {
+
+    }
+    public void reorderList(ListNode head) {
+        // Solution #1 (cut, reverse and merge)
+        if (head == null || head.next == null) {
+            return;
+        }
+        ListNode l1 = head;
+        ListNode slow = head;
+        ListNode fast = head;
+        ListNode prev = null;
+        while (fast != null && fast.next != null) {
+            prev = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        prev.next = null;
+        ListNode l2 = reverse(slow);
+        merge(l1, l2);
+    }
+
+    public ListNode reverse(ListNode head) {
+        ListNode curr = head;
+        ListNode prev = null;
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+
+    public void merge(ListNode l1, ListNode l2) {
+        while (l1 != null) {
+            ListNode l1_next = l1.next;
+            ListNode l2_next = l2.next;
+            l1.next = l2;
+            if (l1_next == null) {
+                break;
+            }
+            l2.next = l1_next;
+            l1 = l1_next;
+            l2 = l2_next;
+        }
+
+    }
+    // Time Complexity: O(n) the first while loop takes O(n / 2) time. The reversing takes O(n / 2) time. And the merging takes O(n) time. Therefore, it will be O(n) overall
+    // Space Complexity: O(1) because we are only using constant extra space
+}
