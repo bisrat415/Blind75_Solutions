2f3d3103bfb215e1803c3ca7f03814c89893bae0
Bisrat Kassie <107273623+bisrat415@users.noreply.github.com>
2023-06-19 12:22:08 -0400

    recursive solution added for validate BST

---
+++ Medium/ValidateBST(Trees)/Solution_1.java
@@ -0,0 +1,27 @@
+// This solution compares the node value with its upper and lower limits if they are available. 
+// Then repeats the same step recursively for left and right subtrees
+
+public class Solution_1 {
+    public static void main(String[] args) {
+
+    }
+
+    public static boolean isValidBST(TreeNode root) {
+        return validate(root, null, null);
+    }
+
+    public static boolean validate(TreeNode root, Integer left, Integer right) {
+        if (root == null) {
+            return true;
+        }
+        if ((left != null && left >= root.val) || (right != null && right <= root.val)) {
+            return false;
+        }
+        return validate(root.left, left, root.val) && validate(root.right, root.val, right);
+        // Let n be the number of nodes in the tree and h be height of the tree
+        // Time Complexity: O(n) because we are visting every node once
+        // Space Complexity: O(h) - O(n) in the worst case (skewed tree) and O(logn) in
+        // the best case (balanced tree)
+    }
+
+}
