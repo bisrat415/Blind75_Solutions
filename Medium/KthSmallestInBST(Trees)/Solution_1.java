3cd73e4a013215d92a7ed5dbca76766da1b8acf2
Bisrat Kassie <107273623+bisrat415@users.noreply.github.com>
2023-06-20 10:17:18 -0400

    solution 1 added for kth smallest in BST

--- Medium/KthSmallestInBST(Trees)/Solution_1.java
+++ Medium/KthSmallestInBST(Trees)/Solution_1.java
@@ -1,4 +1,38 @@
-
+// This is a recursive solution that traverses the BST inorder and adds each element into an arraylist and returns the (k-1) element in the arraylist 
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+import java.util.*;
 public class Solution_1 {
-    
+    public static void main(String[] args) {
+
+    }
+    public static int kthSmallest(TreeNode root, int k) {
+        List<Integer> list = new ArrayList<>();
+        inorder(root, list);
+        return list.get(k - 1);
+    }
+    public static void inorder(TreeNode root, List<Integer> list) {
+        if (root == null) {
+            return;
+        }
+        inorder(root.left, list);
+        list.add(root.val);
+        inorder(root.right, list);
+        // Let n be the number of nodes in the tree and h be the height of the tree
+        // Time Complexity: O(n) because we are visiting each node once
+        // Space Complexity: O(h) - O(n) in the worst case (skewed tree) and O(logn) in the best case / average case (balanced tree)
+    }
 }
