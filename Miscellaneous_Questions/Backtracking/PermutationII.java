e44b2d188422d345e351b95abdd044614475d77c
Bisrat Kassie <107273623+bisrat415@users.noreply.github.com>
2023-06-24 17:55:34 -0400

    solution#2 added for permutationII in Miscellaneous

--- Miscellaneous_Questions/Backtracking/PermutationII.java
+++ Miscellaneous_Questions/Backtracking/PermutationII.java
@@ -3,17 +3,20 @@
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class PermutationII {
     public static void main(String[] args) {
-        int[] nums = {1, 1, 2};
+        int[] nums = { 1, 1, 2 };
         System.out.println(permuteUnique(nums));
+        System.out.println(permuteUnique2(nums));
 
     }
 
+    // Solution #1 (sort)
     public static List<List<Integer>> permuteUnique(int[] nums) {
-        // Solution #1 (sort)
         Arrays.sort(nums);
         List<List<Integer>> result = new ArrayList<>();
         List<Integer> perm = new ArrayList<>();
@@ -43,6 +46,39 @@ public class PermutationII {
     // And for each permutation it takes O(n) time to add to results (because we are
     // creating a new arraylist)
     // And sorting is O(nlogn) < o(n*n!) so overall it is O(n*n!)
+
+    // Space Complexity: O(n) - the max depth of each recursive call (the height of
+    // the call stack) is O(n) and the boolean array 'used' takes O(n) time as well
+    // We don't take result into account for calculating space complexity because it
+    // is part of the output
+
+    // Solution #2 (hashset)
+    public static List<List<Integer>> permuteUnique2(int[] nums) {
+        List<Integer> perm = new ArrayList<>();
+        boolean[] used = new boolean[nums.length];
+        Set<List<Integer>> set = new HashSet<>();
+        permuteUniqueHelper2(nums, perm, set, used);
+        return new ArrayList(set);
+    }
+
+    public static void permuteUniqueHelper2(int[] nums, List<Integer> perm, Set<List<Integer>> set, boolean[] used) {
+        if (perm.size() == nums.length) {
+            set.add(new ArrayList(perm));
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (!used[i]) {
+                perm.add(nums[i]);
+                used[i] = true;
+                permuteUniqueHelper2(nums, perm, set, used);
+                perm.remove(perm.size() - 1);
+                used[i] = false;
+            }
+        }
+    }
+    // Time Complexity: O(n * n!) - There are n! permutations for any given sequence
+    // of n distinct numbers
+    // For each permutation, the algorithm spends O(N) time for adding each
+    // permutation into the hash set (we are copying the arraylist)
 
     // Space Complexity: O(n) - the max depth of each recursive call (the height of
     // the call stack) is O(n) and the boolean array 'used' takes O(n) time as well
