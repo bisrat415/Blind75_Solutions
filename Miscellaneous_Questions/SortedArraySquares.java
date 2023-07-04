659c18fa1d0bb8a8b9a0cb37156affba83697d42
Bisrat Kassie <107273623+bisrat415@users.noreply.github.com>
2023-07-04 11:37:38 -0400

    second solution added for Sorted Array Squares

--- Miscellaneous_Questions/SortedArraySquares.java
+++ Miscellaneous_Questions/SortedArraySquares.java
@@ -8,6 +8,7 @@ public class SortedArraySquares {
     public static void main(String[] args) {
         int[] nums = {-4,-1,0,3,10};
         System.out.println(Arrays.toString(sortedSquares(nums)));
+        System.out.println(Arrays.toString(sortedSquares2(nums)));
     }
 
     // Solution #1 (sort)
@@ -21,6 +22,29 @@ public class SortedArraySquares {
         return squared;
         // Let n be the the length of the input array, nums
         // Time Complexity: O(nlogn) - the forloop takes linear time because we are just traversing through the input array once. And the sorting takes O(nlogn) so we take the dominant time
+        // Space Complexity: O(1) - the output array (squared) will not be considered for space complexity analysis. And for the rest, we are only using constant extra space
+    }
+
+    // Solution #2 (two-pointers)
+    public static int[] sortedSquares2(int[] nums) {
+        int n = nums.length;
+        int[] squared = new int[n];
+        int left = 0;
+        int right = n - 1;
+        for (int i = n - 1; i >= 0; i--) {
+            int nextNum = 0;
+            if (Math.abs(nums[right]) > Math.abs(nums[left])) {
+                nextNum = nums[right];
+                right--;
+            } else {
+                nextNum = nums[left];
+                left++;
+            }
+            squared[i] = nextNum * nextNum;
+        }
+        return squared;
+        // Let n be the the length of the input array, nums
+        // Time Complexity: O(n) - we have one forloop that goes through the array once and all the operations in the loop are constant time
         // Space Complexity: O(1) - the output array (squared) will not be considered for space complexity analysis. And for the rest, we are only using constant extra space
     }
 }
