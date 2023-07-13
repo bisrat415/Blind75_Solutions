f1f9f10ec4362ab0b05ce43943d88bba8733ac2b
Bisrat Kassie <107273623+bisrat415@users.noreply.github.com>
2023-07-13 10:08:15 -0400

    solution 1 added for decode ways

---
+++ Medium/Decode_Ways(Dynamic_Programming)/Solution_1.java
@@ -0,0 +1,29 @@
+// This solution uses recursion with memoization to solve the problem
+public class Solution_1 {
+    public static void main(String[] args) {
+        String s = "226";
+        System.out.println(numDecodings(s));
+    }
+     public static int numDecodings(String s) {
+        return numDecodeHelper(s, 0);
+    }
+    public static int numDecodeHelper(String s, int i) {
+        if (i == s.length()) {
+            return 1;
+        }
+        if (s.charAt(i) == '0') {
+            return 0;
+        }
+        if (i == s.length() - 1) {
+            return 1;
+        }
+        int ans = numDecodeHelper(s, i + 1);
+        if (Integer.parseInt(s.substring(i, i + 2)) <= 26) {
+            ans += numDecodeHelper(s, i + 2);
+        }
+        return ans;
+    }
+    // Let n be the length of the string
+    // Time Complexity: O(n) because we are calling the recursive method for each character in the string and we are using memoization for any overlapping operations (decoding for a specific index only once)
+    // Space Complexity: O(n) because of memo and the recursive call stack which each of them take O(n) space in the worst case
+}
