2b4c2d9a270296442dffad46a7e14f1c16e0e0df
Bisrat Kassie <107273623+bisrat415@users.noreply.github.com>
2023-07-29 17:26:31 -0400

    solution 2 added for parallel courses

--- Miscellaneous_Questions/Graphs/ParallelCourses.java
+++ Miscellaneous_Questions/Graphs/ParallelCourses.java
@@ -4,9 +4,15 @@ public class ParallelCourses {
     public static void main(String[] args) {
         int[][] relations1 = {{1, 3}, {2, 3}};
          int[][] relations2 = {{1, 3}, {2, 3}, {3, 1}};
+         // For Solution #1
          System.out.println(minimumSemesters(0, relations1)); // prints 2
          System.out.println(minimumSemesters(0, relations2)); // prints -1
+          // For Solution #2
+          System.out.println(minimumSemesters2(0, relations1)); // prints 2
+         System.out.println(minimumSemesters2(0, relations2)); // prints -1
     }
+
+    // Solution #1 (use arraylist for adjList and array for indegree)
     public static int minimumSemesters(int n, int[][] relations) {
         List<List<Integer>> adjList = new ArrayList<>();
         for (int i = 0; i < n; i++) {
@@ -41,6 +47,46 @@ public class ParallelCourses {
         }
         return -1;
         // Let n be the number of courses (vertices) and e be the number of relations (edges)
+        // Time Complexity: O(n + e) - creating adjList takes O(n + e) and the BFS takes O(n + e) since have to visit every node and edge once in the worst case
+        // Space Complexity: O(n + e) - adjList takes O(n + e) space and the size of the queue at a specific instance is bounded by the total number of nodes (n)
+    }
+
+    // Solution #2 (uses HashMap for both adjList and indegree)
+    public static int minimumSemesters2(int n, int[][] relations) {
+        Map<Integer, List<Integer>> adjList = new HashMap<>();
+        Map<Integer, Integer> indegree = new HashMap<>();
+        for (int[] relation : relations) {
+            adjList.computeIfAbsent(relation[0], k -> new ArrayList<>()).add(relation[1]);
+            indegree.put(relation[1], indegree.getOrDefault(relation[1], 0) + 1);
+        }
+
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 1; i <= n; i++) {
+            if (!indegree.containsKey(i)) {
+                queue.offer(i);
+            }
+        }
+        int numSemesters = 0;
+        Set<Integer> set = new HashSet<>();
+        while (!queue.isEmpty()) {
+            int s = queue.size();
+            numSemesters++;
+            while(s > 0) {
+                int currNode = queue.poll();
+                set.add(currNode);
+                if (adjList.containsKey(currNode)) {
+                    for (int neighbor : adjList.get(currNode)) {
+                        indegree.put(neighbor, indegree.getOrDefault(neighbor, 0) - 1);
+                        if (indegree.get(neighbor) == 0) {
+                            queue.offer(neighbor);
+                        }
+                    }
+                } 
+                s--;
+            }
+        }
+        return set.size() == n ? numSemesters : -1;
+         // Let n be the number of courses (vertices) and e be the number of relations (edges)
         // Time Complexity: O(n + e) - creating adjList takes O(n + e) and the BFS takes O(n + e) since have to visit every node and edge once in the worst case
         // Space Complexity: O(n + e) - adjList takes O(n + e) space and the size of the queue at a specific instance is bounded by the total number of nodes (n)
     }
