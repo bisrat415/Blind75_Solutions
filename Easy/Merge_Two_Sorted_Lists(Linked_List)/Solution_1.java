cd19d1edf108e1045afd39ccba95da6469598b2f
Bisrat Kassie <107273623+bisrat415@users.noreply.github.com>
2023-08-23 20:18:44 -0700

    comments added for solution 1 for merge two sorted lists

--- Easy/Merge_Two_Sorted_Lists(Linked_List)/Solution_1.java
+++ Easy/Merge_Two_Sorted_Lists(Linked_List)/Solution_1.java
@@ -1,4 +1,14 @@
 // This solution uses an interative approach to merge the two lists
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 public class Solution_1 {
     public static void main(String[] args) {
         
@@ -23,4 +33,7 @@ public class Solution_1 {
         }
         return tempHead.next;
     }
+    // Let m be the number of nodes in list1 and n be the number of nodes in list2
+    // Time complexity: O(m + n) - because we are visiting each node in both lists once and all the other operations are constant time
+    // Space Complexity: O(1) because we are using constant extra space assuming that we are not considering the output list in our space complexity analysis
 }
