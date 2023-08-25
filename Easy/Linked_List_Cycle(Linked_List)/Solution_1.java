21962262bd6fd15bfcf9630acd37a59e41c5092c
Bisrat Kassie <107273623+bisrat415@users.noreply.github.com>
2023-08-25 15:43:37 -0700

    solution 1 added for linked list cycle

---
+++ Easy/Linked_List_Cycle(Linked_List)/Solution_1.java
@@ -0,0 +1,28 @@
+// This is a solution that uses a hashset to figure out if there is a cycle or not
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution_1 {
+    public static void main(String[] args) {
+        
+    }
+    public boolean hasCycle(ListNode head) {
+        HashSet<ListNode> set = new HashSet<>();
+        while (head != null) {
+            if (set.contains(head)) {
+                return true;
+            }
+            set.add(head);
+            head = head.next;
+        }
+        return false;
+    }
+}
