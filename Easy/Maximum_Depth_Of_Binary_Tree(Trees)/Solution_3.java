973492cf28243aae97b20b8e55aa60c89a392ebd
Bisrat Kassie <107273623+bisrat415@users.noreply.github.com>
2023-09-01 09:18:55 -0400

    solution 3 added for maximum depth of binary tree

---
+++ Easy/Maximum_Depth_Of_Binary_Tree(Trees)/Solution_3.java
@@ -0,0 +1,37 @@
+// This is a recursive top-down solution
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution_3 {
+    private int answer;
+    public static void main(String[] args) {
+        
+    }
+    public int maxDepth(TreeNode root) {
+        maxDepthHelper(root, 1);
+        return answer;
+    }
+
+    public void maxDepthHelper(TreeNode root, int depth) {
+        if (root == null) {
+            return;
+        }
+        if (root.left == null && root.right == null) {
+            answer = Math.max(answer, depth);
+        }
+        maxDepthHelper(root.left, depth + 1);
+        maxDepthHelper(root.right, depth + 1);
+    }
+}
